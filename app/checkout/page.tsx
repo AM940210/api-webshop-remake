95760af0af1a8803f4e1f00952ba971c2945a31b
ahmad mohammadi <ahmmoh0210@gmail.com>
2025-06-09 05:42:25 +0200

    tabort en checkout h1

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,7 +6,6 @@ export default function CheckoutPage() {
   return (
     <main>
       <div className="max-w-2xl mx-auto mt-20">
-        <h1 className="text-2xl font-bold mb-6">Checkout</h1>
         <Checkout />
       </div>
     </main>
